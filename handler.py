b415f28e5d448f508a01af1bfc39c892429b02db
Arteww <arrmf1@mail.ru>
2022-06-05 16:46:47 +0700

    VezdecodeInit

--- handler.py
+++ handler.py
@@ -3,18 +3,19 @@ import json
 
 def webhook(event, context):
     request_message = json.loads(event['body'])
-    derived_session_fields = ['session_id', 'user_id', 'message_id']
-    response_message = {
-        "response": {
-            "text": request_message['request']['original_utterance'],
-            "tts": request_message['request']['original_utterance'],
-            "end_session": False
-        },
-        "session": {derived_key: request_message['session'][derived_key] for derived_key in derived_session_fields},
-        "version": request_message['version']
-    }
+    if ' '.join(request_message['request']['nlu']['tokens']) in ["шишки вездеход", "вездеход шишки"]:
+        derived_session_fields = ['session_id', 'user_id', 'message_id']
+        response_message = {
+            "response": {
+                "text": "Привет вездекодерам!",
+                "tts": "Привет вездекодерам!",
+                "end_session": False
+            },
+            "session": {derived_key: request_message['session'][derived_key] for derived_key in derived_session_fields},
+            "version": request_message['version']
+        }
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(response_message)
-    }
+        return {
+            "statusCode": 200,
+            "body": json.dumps(response_message)
+        }
